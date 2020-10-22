f8153b71e80152dd4fab5a53103e45a93c6748cb
fichorka <fichorka@gmail.com>
2020-10-22 15:15:13 +0200

    Initial commit

---
+++ Server/Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace Server.Models
+{
+    public class Department
+    {
+        public long Id { get; set; }
+        public string DepartmentName { get; set; }
+        public string DepartmentLocation { get; set; }
+    }
+}
---
+++ Server/Models/Login.cs
@@ -0,0 +1,9 @@
+﻿namespace Server.Models
+{
+    public class Login
+    {
+        public long Id { get; set; }
+        public string LoginUserName { get; set; }
+        public string LoginPassword { get; set; }
+    }
+}
