fa0e05a16c5a111c13e84bbd2e65a335b877079d
fichorka <fichorka@gmail.com>
2020-10-23 19:21:45 +0200

    Fix custom DB initializer

--- Api/Models/CompanyDbContext.cs
+++ Api/Models/CompanyDbContext.cs
@@ -32,28 +32,28 @@ namespace Api.Models
 
             IList<Department> defaultDepartments = new List<Department>();
 
-            Department dep1 = new Department() { DepartmentName = "Development", DepartmentLocation = "Location" };
-            Department dep2 = new Department() { DepartmentName = "Development", DepartmentLocation = "Zurich" };
-            Department dep3 = new Department() { DepartmentName = "Development", DepartmentLocation = "Osijek" };
-            Department dep4 = new Department() { DepartmentName = "Sales", DepartmentLocation = "London" };
-            Department dep5 = new Department() { DepartmentName = "Sales", DepartmentLocation = "Zurich" };
-            Department dep6 = new Department() { DepartmentName = "Sales", DepartmentLocation = "Osijek" };
-            Department dep7 = new Department() { DepartmentName = "Sales", DepartmentLocation = "Basel" };
-            Department dep8 = new Department() { DepartmentName = "Sales", DepartmentLocation = "Lugano" };
+            defaultDepartments.Add(new Department() { DepartmentName = "Development", DepartmentLocation = "Location" });
+            defaultDepartments.Add(new Department() { DepartmentName = "Development", DepartmentLocation = "Zurich" });
+            defaultDepartments.Add(new Department() { DepartmentName = "Development", DepartmentLocation = "Osijek" });
+            defaultDepartments.Add(new Department() { DepartmentName = "Sales", DepartmentLocation = "London" });
+            defaultDepartments.Add(new Department() { DepartmentName = "Sales", DepartmentLocation = "Zurich" });
+            defaultDepartments.Add(new Department() { DepartmentName = "Sales", DepartmentLocation = "Osijek" });
+            defaultDepartments.Add(new Department() { DepartmentName = "Sales", DepartmentLocation = "Basel" });
+            defaultDepartments.Add(new Department() { DepartmentName = "Sales", DepartmentLocation = "Lugano" });
 
             context.Department.AddRange(defaultDepartments);
 
             IList<Employee> defaultEmployees = new List<Employee>();
 
-            Employee emp1 = new Employee() { EmployeeName = "Fred Davies", Salary = 50000, DepartmentNo = dep4.DepartmentNo };
-            Employee emp2 = new Employee() { EmployeeName = "Bernard Katic", Salary = 50000, DepartmentNo = dep3.DepartmentNo };
-            Employee emp3 = new Employee() { EmployeeName = "Rich Davies", Salary = 30000, DepartmentNo = dep5.DepartmentNo };
-            Employee emp4 = new Employee() { EmployeeName = "Eva Dobos", Salary = 30000, DepartmentNo = dep6.DepartmentNo };
-            Employee emp5 = new Employee() { EmployeeName = "Mario Hunjadi", Salary = 25000, DepartmentNo = dep8.DepartmentNo };
-            Employee emp6 = new Employee() { EmployeeName = "Jean Michele", Salary = 25000, DepartmentNo = dep7.DepartmentNo };
-            Employee emp7 = new Employee() { EmployeeName = "Bill Gates", Salary = 25000, DepartmentNo = dep1.DepartmentNo };
-            Employee emp8 = new Employee() { EmployeeName = "Maja Janic", Salary = 30000, DepartmentNo = dep3.DepartmentNo };
-            Employee emp9 = new Employee() { EmployeeName = "Igor Horvat", Salary = 35000, DepartmentNo = dep3.DepartmentNo };
+            defaultEmployees.Add(new Employee() { EmployeeName = "Fred Davies", Salary = 50000, DepartmentNo = defaultDepartments[3].DepartmentNo });
+            defaultEmployees.Add(new Employee() { EmployeeName = "Bernard Katic", Salary = 50000, DepartmentNo = defaultDepartments[2].DepartmentNo });
+            defaultEmployees.Add(new Employee() { EmployeeName = "Rich Davies", Salary = 30000, DepartmentNo = defaultDepartments[4].DepartmentNo });
+            defaultEmployees.Add(new Employee() { EmployeeName = "Eva Dobos", Salary = 30000, DepartmentNo = defaultDepartments[5].DepartmentNo });
+            defaultEmployees.Add(new Employee() { EmployeeName = "Mario Hunjadi", Salary = 25000, DepartmentNo = defaultDepartments[7].DepartmentNo });
+            defaultEmployees.Add(new Employee() { EmployeeName = "Jean Michele", Salary = 25000, DepartmentNo = defaultDepartments[6].DepartmentNo });
+            defaultEmployees.Add(new Employee() { EmployeeName = "Bill Gates", Salary = 25000, DepartmentNo = defaultDepartments[0].DepartmentNo });
+            defaultEmployees.Add(new Employee() { EmployeeName = "Maja Janic", Salary = 30000, DepartmentNo = defaultDepartments[2].DepartmentNo });
+            defaultEmployees.Add(new Employee() { EmployeeName = "Igor Horvat", Salary = 35000, DepartmentNo = defaultDepartments[2].DepartmentNo });
 
             context.Employee.AddRange(defaultEmployees);
 
